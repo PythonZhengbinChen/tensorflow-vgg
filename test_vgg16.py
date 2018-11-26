72d3c816490657b5b3d64e96e35c4efda5f39271
unknown <360522450@qq.com>
2018-11-26 13:42:01 +0800

    tensorflow vgg code first commit

---
+++ test_vgg16.py
@@ -0,0 +1,31 @@
+import numpy as np
+import tensorflow as tf
+
+import vgg16
+import utils
+
+img1 = utils.load_image("./test_data/test.jpg")
+img2 = utils.load_image("./test_data/puzzle.jpeg")
+
+batch1 = img1.reshape((1, 224, 224, 3))
+batch2 = img2.reshape((1, 224, 224, 3))
+
+batch = np.concatenate((batch1, batch2), 0)
+
+# with tf.Session(
+#         config=tf.ConfigProto(gpu_options=(tf.GPUOptions(per_process_gpu_memory_fraction=0.7)))) as sess:
+
+with tf.device('/cpu:0'):
+    with tf.Session() as sess:
+        images = tf.placeholder("float", [None, 224, 224, 3])
+        feed_dict = {images: batch}
+
+        vgg = vgg16.Vgg16()
+
+        with tf.name_scope("content_vgg"):
+            vgg.build(images)
+
+        prob = sess.run(vgg.prob, feed_dict=feed_dict)
+
+        utils.print_prob(prob[0], './synset.txt')
+        utils.print_prob(prob[1], './synset.txt')
